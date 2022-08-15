194778b7bd43d1559706ed0ccb3dc88efe2b7c58
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-08-15 11:57:10 +0100

    function

---
+++ function.py
@@ -0,0 +1,9 @@
+def add_five(number):
+  return number + 5
+
+print(add_five(2132))
+
+def subtract_low_from_high(num1, num2):
+  return num1 - num2 if num1 > num2 else num2 - num1
+
+print(add_five(subtract_low_from_high(1463, 16475)))
