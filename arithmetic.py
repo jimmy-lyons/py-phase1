4403c7aedc1e74d78059a970902ff27a98c90906
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-08-15 10:53:09 +0100

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+reminaing_apples = 50 - 23
+
+print(f"Remaining apples = {reminaing_apples}")
+
+print(f"Mins in 2022 = {365 * 24 * 60}")
+
+print(f"Number of tanks = {32452 // 13}")
+
+print(f"Number of herring = {32452 % 13}")
