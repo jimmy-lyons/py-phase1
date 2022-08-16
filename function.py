f67955ff2a9d5803d2d884ed1fc12805e8e68d35
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-08-16 09:13:08 +0100

    superify

--- function.py
+++ function.py
@@ -7,3 +7,8 @@ def subtract_low_from_high(num1, num2):
   return num1 - num2 if num1 > num2 else num2 - num1
 
 print(add_five(subtract_low_from_high(1463, 16475)))
+
+def superify(word):
+  return "super" + word
+
+print(superify("cat"))
